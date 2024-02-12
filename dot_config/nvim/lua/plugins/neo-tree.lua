1ef4cb2b3043b10405eeca7541f23c1cd657be9e
iT3E <26583558+iT3E@users.noreply.github.com>
2024-02-11 18:13:45 -0800

    NEW: Add nvim-web-devicons dep

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     filesystem = {
       filtered_items = {
         visible = true,
