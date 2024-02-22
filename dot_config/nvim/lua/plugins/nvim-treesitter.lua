810cfc0c14f60a9031b754d813a3a548869e2f69
iT3E <26583558+iT3E@users.noreply.github.com>
2024-02-21 21:29:27 -0800

    NEW: Add chezmoi.vim

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  highlight = {
+    disable = function()
+      if string.find(vim.bo.filetype, "chezmoitmpl") then
+        return true
+      end
+    end,
+  },
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, {
