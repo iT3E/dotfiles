3af212546285e17f3ce61b389bcd0308f954c0ef
iT3E <26583558+iT3E@users.noreply.github.com>
2024-03-04 15:32:03 -0800

    IMPROVE: Add yank keys

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,19 @@ vim.keymap.set(
   ":Telescope notify<CR>",
   { noremap = true, silent = true, desc = "Search Notification History" }
 )
+
+vim.keymap.set(
+  "n",
+  "<leader>yp",
+  "<cmd>let @+ = expand('%:~:.')<cr> <cmd>lua require('notify')('Yanked relative path')<cr>",
+  { noremap = true, silent = true, desc = "Relative Path" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>yf",
+  "<cmd>let @+ = expand('%:p')<cr> <cmd>lua require('notify')('Yanked full path')<cr>",
+  { noremap = true, silent = true, desc = "Full Path" }
+)
+
+vim.keymap.set("n", "<leader>ya", "<cmd>%y<cr>", { noremap = true, silent = true, desc = "Buffer Contents" })
