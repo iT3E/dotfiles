2d26f0570c2ea57710515974e5976b91a0acffce
iT3E <26583558+iT3E@users.noreply.github.com>
2024-03-27 21:18:13 -0700

    IMPROVE: Add more gp agent options

--- dot_config/nvim/lua/plugins/gp.lua
+++ dot_config/nvim/lua/plugins/gp.lua
@@ -27,6 +27,31 @@ return {
             .. "- Take a deep breath; You've got this!\n",
         },
         {
+          name = "gpt-4-0125-Preview-lessverbose",
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-4-0125-preview", temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
+        },
+        {
+          name = "gpt-4-0125-Preview",
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-4-0125-preview", temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are a general AI assistant.\n\n",
+        },
+        {
           name = "CodeGPT4-0125-Preview",
           chat = false,
           command = true,
